27a6a1c37d6080ef16b69b68ee7f88deb19e9bb5
HOANGNH <hoangnhonline@gmail.com>
2016-01-25 23:31:11 +0700

    source

---
+++ app/view/menungang.php
@@ -0,0 +1,39 @@
+<div id="thanhmenungang">
+  		<ul>
+  			<li class="active">
+  				<a href="<?php echo BASE_URL?>">Trang chủ</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>chua-thien-quang/" title="Thông báo và các tin tức về chùa Thiên Quang">Chùa Thiên Quang</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>phat-hoc-can-ban/">Phật học căn bản</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>nghe-phap-thoai/" title="Bài giảng của Sư Cô Hương Nhũ">Nghe pháp thoại</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>hat-giong-tam-hon/" title="Các pháp thoại của Sư Cô viết thành chữ">Hạt giống tâm hồn</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>kenh-truyen-hinh-an-vien/">Kênh T.H An Viên</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>khoa-tu-chua-thien-quang/" title="Bài viết về các khoá tu chùa Thiên Quang">Các khoá tu</a>
+  			</li>
+  			<!--<li>
+  				<a href="<?php echo BASE_URL?>bai-hoc-cuoc-song/">Bài học cuộc sống</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>nhac-phat-giao/" title="Nhạc MP3, nhạc video, ca cổ Phật giáo">Nhạc Phật giáo</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>phim-phat-giao/" title="Phim Phật giáo">Phim Phật giáo</a>
+  			</li>
+  			<li>
+  				<a href="<?php echo BASE_URL?>kinh-dien-phat-giao/">Kinh điển Phật giáo</a>
+  			</li>-->
+  		</ul>		
+		<!-- a href="<?php echo BASE_URL?>khiemthi/" title="Trang cho người khiếm thị">Website cho người khiếm thị</a-->
+		</div>
+	
